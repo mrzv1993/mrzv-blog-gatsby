1ea87f48e89a1c4f3ec5db32f0329bbc4c25b838
Stanislav Morozov <stainislav@mrzv.design>
2018-12-26 14:52:44 +1000

    fix: remove import clap

--- src/components/Claps/index.js
+++ src/components/Claps/index.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 import styled from 'styled-components'
-import 'applause-button'
 
 const Claps = styled.div``
 
 export default function() {
   return (
     <Claps>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/applause-button/dist/applause-button.css"
+        />
+        <script src="https://unpkg.com/applause-button/dist/applause-button.js" />
+      </Helmet>
       <applause-button
         style={{ width: '58px', height: '58px' }}
         multiclap="true"
         color="#4a4fd9"
         url="https://mrzv1993.ru/first-post"
-      />
+      >
+        {' '}
+      </applause-button>
     </Claps>
   )
 }
