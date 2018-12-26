92a79023ee4db7388476ef93d27d7987e01fd15b
Stanislav Morozov <stainislav@mrzv.design>
2018-12-26 14:10:12 +1000

    fix: init applause-button

--- src/components/Claps/index.js
+++ src/components/Claps/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import styled from 'styled-components'
+import 'applause-button'
 
 const Claps = styled.div``
 
@@ -15,7 +16,7 @@ export default function() {
         <script src="https://unpkg.com/applause-button/dist/applause-button.js" />
       </Helmet>
       <applause-button
-        style={{ width: '58px', height: '58px', display: 'block' }}
+        style={{ width: '58px', height: '58px' }}
         multiclap="true"
         color="#4a4fd9"
         url="https://mrzv1993.ru/first-post"
