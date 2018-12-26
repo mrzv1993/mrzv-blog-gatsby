4f8c1ca290e3fd70adf35cca58207a749cd8f50a
Stanislav Morozov <stainislav@mrzv.design>
2018-12-26 13:11:51 +1000

    fix: add claps

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 
 import Layout from './layout'
 
 const Template = props => {
   return (
     <Layout>
-      <p>sad</p>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/applause-button/dist/applause-button.css"
+        />
+        <script src="https://unpkg.com/applause-button/dist/applause-button.js" />
+      </Helmet>
+      <p>sada</p>
+      <applause-button
+        style={{ width: '58px', height: '58px', display: 'block' }}
+        multiclap="true"
+        color="#4a4fd9"
+      />
     </Layout>
   )
 }
