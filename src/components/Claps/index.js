6860d2dae626cd6145701e6428c54568441d3d61
Stanislav Morozov <stainislav@mrzv.design>
2018-12-26 14:15:08 +1000

    fix: edit claps again

--- src/components/Claps/index.js
+++ src/components/Claps/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Helmet from 'react-helmet'
 import styled from 'styled-components'
 import 'applause-button'
 
@@ -8,13 +7,6 @@ const Claps = styled.div``
 export default function() {
   return (
     <Claps>
-      <Helmet>
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/applause-button/dist/applause-button.css"
-        />
-        <script src="https://unpkg.com/applause-button/dist/applause-button.js" />
-      </Helmet>
       <applause-button
         style={{ width: '58px', height: '58px' }}
         multiclap="true"
