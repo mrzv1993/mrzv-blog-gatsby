1e0a9b74613dd594251b64a661041ea353219b02
Stanislav Morozov <stainislav@mrzv.design>
2018-12-26 15:13:44 +1000

    fix: add post's content

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,13 +1,96 @@
 import React from 'react'
-
+import { graphql } from 'gatsby'
 import Layout from './layout'
+import styled from 'styled-components'
+import Moment from 'react-moment'
+import 'moment/locale/ru'
+import Helmet from 'react-helmet'
 
-const Template = props => {
+const Time = styled(Moment)`
+  font-family: 'Fira Mono', monospace;
+  font-weight: 400;
+  color: #6c7480;
+  font-size: 14px;
+  margin-top: 32px;
+`
+
+const Title = styled.h1`
+  font-size: 40px;
+  text-align: center;
+  font-family: 'Source Sans Pro', sans-serif;
+  font-weight: 700;
+  margin-top: 32px;
+  color: #354356;
+`
+
+const BlogPost = styled.div`
+  margin-top: 32px;
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    max-width: 640px;
+    margin: 0 auto;
+    font-family: 'Fira Mono', monospace;
+    font-size: 17px;
+    line-height: 28px;
+    color: #354356;
+  }
+`
+const Clap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 48px 0;
+`
+
+const Template = ({ data }) => {
+  const { markdownRemark } = data
+  const title = markdownRemark.frontmatter.title
+  const date = markdownRemark.frontmatter.date
+  const html = markdownRemark.html
   return (
     <Layout>
-      <p>sada</p>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/applause-button/dist/applause-button.css"
+        />
+        <script src="https://unpkg.com/applause-button/dist/applause-button.js" />
+      </Helmet>
+      <Time format="D MMM YYYY" withTitle locale="ru">
+        {date}
+      </Time>
+      <Title>{title}</Title>
+      <BlogPost dangerouslySetInnerHTML={{ __html: html }} />
+      <Clap>
+        <applause-button
+          style={{ width: '58px', height: '58px', display: 'block' }}
+          multiclap="true"
+          color="#4a4fd9"
+        />
+      </Clap>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+      html
+      frontmatter {
+        title
+        date
+        tags
+        path
+        description
+        summary
+        image
+      }
+    }
+  }
+`
 
 export default Template
